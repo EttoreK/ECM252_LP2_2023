f025b3261ac9f7d430a3935a371a431647a27e21
ETTORE PADULA DALBEN <20.00387-0@maua.br>
2023-02-25 09:16:13 -0300

    Fim da 1 Aula

--- Aula 2 - JavaScript/script.js
+++ Aula 2 - JavaScript/script.js
@@ -5,52 +5,89 @@
 
 // ===================================
 
-// const nome1 = "Ana"
-// const nome2 = 'Pedro'
-// const nome3 = `jose`
+/* 
+const nome1 = "Ana"
+const nome2 = 'Pedro'
+const nome3 = `jose`
 
-// console.log(typeof(nome1))
-// console.log(typeof(nome2))
-// console.log(typeof(nome3))
+console.log(typeof(nome1))
+console.log(typeof(nome2))
+console.log(typeof(nome3))
 
-// nome1 = "joao"
-
-// ===================================
-
-// let a = 2
-// let b = "abc"
-
-// console.log(typeof(b))
-
-// b = 12
-// console.log(typeof(b))
-
-// b = true
-// console.log(typeof(b))
+nome1 = "joao"
+ */
 
 // ===================================
 
-// var endereco = "Rua C"
-// console.log(endereco)
-// endereco = "Rua B"
-// console.log(endereco)
-// console.log(typeof(endereco))
+/* 
+let a = 2
+let b = "abc"
+
+console.log(typeof(b))
+
+b = 12
+console.log(typeof(b))
+
+b = true
+console.log(typeof(b)) 
+*/
 
 // ===================================
 
-// var linguagem = "JavaScript"
-// console.log("Aprendendo " + linguagem)
-// var linguagem = "Java"
-// console.log("Aprendendo " + linguagem)
+/* 
+var endereco = "Rua C"
+console.log(endereco)
+endereco = "Rua B"
+console.log(endereco)
+console.log(typeof(endereco)) 
+*/
 
 // ===================================
 
-// var idade = 18
-// console.log("Oi, " + nome)
-// if(idade >= 18){
-//     var nome = "João"
-//     console.log(nome + ", você pode dirigir")
-// }
-// console.log("Até mais, " + nome)
+/*
+var linguagem = "JavaScript"
+console.log("Aprendendo " + linguagem)
+var linguagem = "Java"
+console.log("Aprendendo " + linguagem)
+*/
 
 // ===================================
+
+/*
+var idade = 18
+console.log("Oi, " + nome)
+if(idade >= 18){
+    var nome = "João"
+    console.log(nome + ", você pode dirigir")
+}
+console.log("Até mais, " + nome)
+*/
+
+// ===================================
+
+/*
+const n1 = 2
+const n2 = '3'
+const n3 = n1 + n2 //Coersão implicita
+console.log(n3)
+const n4 = n1 + Number(n2) //Coersão explicita
+*/
+
+// ===================================
+
+/*
+console.log(1 == 1)
+console.log(1 == '1')
+
+console.log(1 === 1)
+console.log(1 === "1")
+*/
+
+// ===================================
+
+console.log(true == 1)
+console.log([1] == 1)
+console.log(null == null)
+console.log(null == undefined)
+console.log([] == false)
+console.log([] == [])
