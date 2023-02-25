babaa6599885055e1ea3e0f1d91b6a9390a90d90
ETTORE PADULA DALBEN <20.00387-0@maua.br>
2023-02-25 08:20:58 -0300

    inicio de ~programação

---
+++ Aula 2 - JavaScript/script.js
@@ -0,0 +1,25 @@
+// para executar: node + arquivo
+
+//const, let, var
+//REPL, Read, Evaluate, Print, Loop
+
+// const nome1 = "Ana"
+// const nome2 = 'Pedro'
+// const nome3 = `jose`
+
+// console.log(typeof(nome1))
+// console.log(typeof(nome2))
+// console.log(typeof(nome3))
+
+// nome1 = "joao"
+
+let a = 2
+let b = "abc"
+
+console.log(typeof(b))
+
+b = 12
+console.log(typeof(b))
+
+b = true
+console.log(typeof(b))
