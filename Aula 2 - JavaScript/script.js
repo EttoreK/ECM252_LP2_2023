ff2e8e9b93699cd5274f3ce13b873f579ead45c2
ETTORE PADULA DALBEN <20.00387-0@maua.br>
2023-03-04 09:15:03 -0300

    fim da aula 3

--- Aula 2 - JavaScript/script.js
+++ Aula 2 - JavaScript/script.js
@@ -85,9 +85,89 @@ console.log(1 === "1")
 
 // ===================================
 
+/*
 console.log(true == 1)
 console.log([1] == 1)
 console.log(null == null)
 console.log(null == undefined)
 console.log([] == false)
 console.log([] == [])
+*/
+
+// ===================================
+
+/*
+v1 = []
+v1[0] = 3.4
+v1[10] = 2
+console.log(v1)
+for(let i = 0; 1 < v1.length; i++)
+    console.log(v1[i])
+*/
+
+// ===================================
+
+/* 
+function soma(a, b){
+    return(
+        a + b
+    );
+}
+
+console.log(2, 3)
+*/
+
+// ===================================
+
+/*
+const nomes = ['Namaria', "Tonho", "Drigo", "Alex", "Crista"];
+const resultado = nomes.filter((nome) => {return nome.startsWith('A')})
+
+console.log(resultado)
+
+const letrasIniciais = nomes.map((nome) => {return nome.charAt(0)})
+console.log(letrasIniciais)
+
+const numeros = [1, 2, 3, 4, 5]
+const qua = numeros.map((num) => {return num**2})
+console.log(qua)
+
+const soma = valores.reduce((ac, v) => {return ac + v})
+console.log(soma)
+*/
+
+// ===================================
+
+function hello(){
+    console.log("Olá meu Chapa")
+}
+hello()
+
+function hello(nome){
+    console.log("Olá meu Chapa" + nome)
+}
+hello("Nat")
+
+function dobro(num){ 
+    return num * 2;
+}
+
+function triplo(num){ 
+    return num * 3;
+}
+
+console.log(triplo(4))
+console.log(dobro(5))
+
+const teste1 = (a, b) => (a + b)
+const teste2 = (a, b, c) => (a * b * c)
+
+console.log(teste1("a", "b"))
+console.log(teste2(1, 5, 6))
+
+const hello2 = () => console.log("Hello")
+console.log(hello2())
+
+const epar = (n) => 
+    n%2 === 0;
+console.log(epar(3))
