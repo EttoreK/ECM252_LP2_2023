5530959989e8d28d89752469d72bbb2f86188497
ETTORE PADULA DALBEN <20.00387-0@maua.br>
2023-03-11 09:15:11 -0300

    Fim do capitulo 2

--- Aula 2 - JavaScript/script.js
+++ Aula 2 - JavaScript/script.js
@@ -138,6 +138,7 @@ console.log(soma)
 
 // ===================================
 
+/*
 function hello(){
     console.log("Olá meu Chapa")
 }
@@ -171,3 +172,161 @@ console.log(hello2())
 const epar = (n) => 
     n%2 === 0;
 console.log(epar(3))
+*/
+
+// ===================================
+
+/*
+let umaFuncao = function () {
+    console.log ("Fui armazenada em uma variável");
+}
+umaFuncao()
+function f (funcao) {
+    funcao()
+}
+function g () {
+    function outraFuncao(){
+        console.log("Fui criada por g");
+    }
+    return outraFuncao;
+}
+f (function (){
+    console.log ('Estou sendo passada para f')
+})
+const gResult = g()
+gResult()
+g()()
+f(g)
+f(g())
+f(g()())
+f(1)
+*/
+
+// ===================================
+
+/*
+function f () {
+    let nome = 'João';
+    function g () {
+        console.log (nome);
+    }
+    g()
+}
+f()
+*/
+
+// ===================================
+
+/*
+function ola(){
+    let nome = 'João';
+    return function (){
+        console.log ('Olá, João');
+    }
+}
+let olaResult = ola();
+olaResult();
+function saudacoesFactory(saudacao, nome){
+    return function (){
+        console.log (saudacao + ', ' + nome);
+    }
+}
+let olaJoao = saudacoesFactory ('Olá', 'João');
+let tchauJoao = saudacoesFactory('Tchau', 'João');
+olaJoao();
+tchauJoao();
+*/
+
+// ===================================
+
+/*
+function eAgora(){
+    let cont = 1;
+    function f1 (){
+        console.log (cont);
+    }
+    cont++;
+    function f2 (){
+        console.log (cont);
+    }
+    return {f1, f2}
+}
+let eAgoraResult = eAgora();
+eAgoraResult.f1();
+eAgoraResult.f2();
+*/
+
+// ===================================
+
+/*
+let pessoa = {
+    nome: "João",
+    idade: 17,
+}
+console.log("Me chamo " + pessoa.nome);
+console.log("Tenho " + pessoa["idade"] + " anos");
+*/
+
+// ===================================
+
+/*
+let pessoaComEndereco = {
+    nome: "Maria",
+    idade: 21,
+    endereco: {
+        logradouro: "Rua B",
+        numero: 121,
+    },
+};
+console.log(
+    `Sou ${pessoaComEndereco.nome},
+    tenho ${pessoaComEndereco.idade} anos
+    e moro na rua ${pessoaComEndereco.endereco["logradouro"]}
+    número ${pessoaComEndereco["endereco"]["numero"]}`
+);
+*/
+
+// ===================================
+
+/*
+let concessionaria = {
+    cnpj: "00011122210001-45",
+    endereco: {
+        logradouro: "Rua A",
+        numero: 10,
+        bairro: "Vila J",
+    },
+    veiculos: [{
+            marca: "Ford",
+            modelo: "Ecosport",
+            anoDeFabricacao: 2018,
+        },
+        {
+            marca: "Chevrolet",
+            modelo: "Onix",
+            anoDeFabricacao: 2020,
+        },
+        {
+            marca: "Volkswagen",
+            modelo: "Nivus",
+            anoDeFabricacao: 2020,
+        },
+    ],
+};
+for (let veiculo of concessionaria.veiculos) {
+    console.log(`Marca: ${veiculo.marca}`);
+    console.log(`Modelo: ${veiculo.modelo}`);
+    console.log(`Ano de Fabricação:
+    ${veiculo.anoDeFabricacao}`);
+}
+*/
+
+// ===================================
+
+let calc = {
+    somar: (a, b) => a + b,
+    subtrair: (a, b) => a - b
+}
+
+console.log(calc.somar(2,6))
+console.log(calc.subtrair(2,6))
